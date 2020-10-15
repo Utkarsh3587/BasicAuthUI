4e576ac92664d85e63bd1d20cfcf84ab2dd6e9fc
Utkarsh Soni <soniutkarsh91@gmail.com>
2020-10-16 03:07:59 +0530

    feat: Add API service

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import {environment} from '../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    private apiUrl: string = environment.apiURL;
+    private headers: any;
+
+    constructor(private http: HttpClient) {
+        this.headers = { headers: new HttpHeaders(
+          { 'Content-Type': 'application/json'}) };
+    }
+
+    getTenCrops(): Observable<any>{
+      return this.http.get<any>(this.apiUrl + 'crops/').pipe();
+    }
+
+    login(userData: any): Observable<any>{
+      return this.http.post<any>(this.apiUrl + 'users/login/', userData, this.headers).pipe();
+    }
+
+    register(userData: any): Observable<any>{
+      return this.http.post<any>(this.apiUrl + 'users/register/', userData, this.headers).pipe();
+    }
+}
